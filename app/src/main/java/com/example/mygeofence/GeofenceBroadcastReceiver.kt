5188ea5e94643f5a0f76b6f28e65bc166588e1b0
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-07-05 12:56:24 +0700

    setup notification

--- app/src/main/java/com/example/mygeofence/GeofenceBroadcastReceiver.kt
+++ app/src/main/java/com/example/mygeofence/GeofenceBroadcastReceiver.kt
@@ -1,9 +1,13 @@
 package com.example.mygeofence
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.android.gms.location.Geofence
 import com.google.android.gms.location.GeofenceStatusCodes
 import com.google.android.gms.location.GeofencingEvent
@@ -18,6 +22,7 @@ class GeofenceBroadcastReceiver : BroadcastReceiver() {
                 val errorMessage =
                     GeofenceStatusCodes.getStatusCodeString(geofencingEvent.errorCode)
                 Log.e(TAG, errorMessage)
+                sendNotification(context, errorMessage)
                 return
             }
 
@@ -44,8 +49,33 @@ class GeofenceBroadcastReceiver : BroadcastReceiver() {
         }
     }
 
+    private fun sendNotification(context: Context, geofenceTransitionDetails: String) {
+        val mNotificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val mBuilder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setContentTitle(geofenceTransitionDetails)
+            .setContentText("Anda sudah bisa absen sekarang :)")
+            .setSmallIcon(R.drawable.baseline_notifications_active_24)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel =
+                NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH)
+            mBuilder.setChannelId(CHANNEL_ID)
+            mNotificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = mBuilder.build()
+
+        mNotificationManager.notify(NOTIFICATION_ID, notification)
+
+    }
+
     companion object {
         private const val TAG = "GeofenceBroadcast"
         const val ACTION_GEOFENCE_EVENT = "GeofenceEvent"
+
+        private const val CHANNEL_ID = "1"
+        private const val CHANNEL_NAME = "Geofence Channel"
+        private const val NOTIFICATION_ID = 1
     }
 }
